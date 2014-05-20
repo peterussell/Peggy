c34dda6d7de9e4c98e654f4e3097c0c35f7e05a5
peterussell <pinkbatz@gmail.com>
2014-05-19 19:17:54 -0700

    Added website.

--- website/peggy/game/urls.py
+++ website/peggy/game/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from game import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url(r'^execCmd/$', views.execCmd, name='execCmd'),
 )
--- website/peggy/game/views.py
+++ website/peggy/game/views.py
@@ -1,4 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def index(request):
-	return HttpResponse("Hello, world. You're at Peggy's index")
+	"""
+	Displays the main game page.
+	"""
+	return render(request, 'game/index.html')	
+
+def execCmd(request):
+	"""
+	Executes a player command.
+	"""
+	return render(request, 'game/index.html')
