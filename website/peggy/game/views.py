02af24068883892a76288a1ae63cd679de2fb89e
pete <jeff@ubuntu.(none)>
2014-07-17 01:33:19 -0700

    Added AJAX command submission

--- website/peggy/game/views.py
+++ website/peggy/game/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+#from django.core import json
+import json
 
-def index(request):
+def index( request ):
 	"""
 	Displays the main game page.
 	"""
 	return render(request, 'game/index.html')	
 
-def execCmd(request):
+def execCmd( request ):
 	"""
 	Executes a player command.
 	"""
-	return render(request, 'game/index.html')
+
+	msg = ""
+	errorMsg = ""
+	if request.method == u'GET':
+		if request.GET['cmdValue']:
+			msg = request.GET['cmdValue']
+		else:
+			errorMsg = "Specify a command"
+	else:	
+		msg = "This is not a get request"
+
+	data = json.dumps( { 'message': msg, 'errorMessage': errorMsg });
+	return HttpResponse( data, mimetype='application/json' ) 
